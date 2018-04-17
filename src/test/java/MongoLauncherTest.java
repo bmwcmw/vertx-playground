d641b5437d7f26e4599bf659a1f5fa6d0cc4b594
CMWZ97I <syncpages@gmail.com>
2018-04-17 23:15:47 +0200

    Clean pg project

---
+++ src/test/java/MongoLauncherTest.java
@@ -0,0 +1,31 @@
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class MongoLauncherTest extends TestCase {
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public MongoLauncherTest(String testName) {
+        super(testName);
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite() {
+        return new TestSuite(MongoLauncherTest.class);
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp() {
+        assertTrue(true);
+    }
+}
