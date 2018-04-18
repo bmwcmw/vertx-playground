4e81fe0e90d358efb6b936be8a80cdd13a21dfc4
Minway <syncpages@gmail.com>
2018-04-18 16:46:45 +0200

    Project cleaning and fix api call

--- src/main/java/asyncclient/DemoWebClient.java
+++ src/main/java/asyncclient/DemoWebClient.java
@@ -2,10 +2,15 @@ package asyncclient;
 
 import io.vertx.core.Future;
 import io.vertx.core.Vertx;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.http.HttpHeaders;
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.client.HttpResponse;
 import io.vertx.ext.web.client.WebClient;
 import io.vertx.ext.web.codec.BodyCodec;
+
+import javax.ws.rs.core.MediaType;
 
 public class DemoWebClient {
 
@@ -18,15 +23,20 @@ public class DemoWebClient {
     }
 
     public Future<JsonObject> fetchResult() {
+        // Or CompletableFuture
         Future<JsonObject> future = Future.future();
-        this.client.get(80, "https://jsonplaceholder.typicode.com", "/todos")
-                .as(BodyCodec.jsonObject())
+        this.client.get(443, "jsonplaceholder.typicode.com", "/todos")
+                .ssl(true)
+                .putHeader(HttpHeaders.CONTENT_TYPE.toString(), MediaType.APPLICATION_JSON)
+                .as(BodyCodec.jsonArray())
                 .send(ar -> {
                     if (ar.succeeded()) {
-                        HttpResponse<JsonObject> response = ar.result();
-                        System.out.println("Got HTTP response body by " + this.id);
-                        System.out.println(response.body().encodePrettily());
+                        HttpResponse<JsonArray> response = ar.result();
+                        JsonArray jsonArr = response.body();
+                        System.out.println("Got HTTP response body by " + this.id + " with length " + jsonArr.size());
+                        System.out.println();
                     } else {
+                        System.out.println("Failed by " + this.id);
                         ar.cause().printStackTrace();
                     }
                 });
