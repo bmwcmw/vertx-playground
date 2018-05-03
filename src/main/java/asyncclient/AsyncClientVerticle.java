71cc22b327c800281e5592434b81270db7d97a55
CMWZ97I <syncpages@gmail.com>
2018-05-03 02:05:14 +0200

    Complete ex

--- src/main/java/asyncclient/AsyncClientVerticle.java
+++ src/main/java/asyncclient/AsyncClientVerticle.java
@@ -1,8 +1,13 @@
 package asyncclient;
 
 import io.vertx.core.*;
+import io.vertx.core.json.JsonArray;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.stream.IntStream;
 
 public class AsyncClientVerticle extends AbstractVerticle {
@@ -12,9 +17,9 @@ public class AsyncClientVerticle extends AbstractVerticle {
     public static void main(String[] args) {
 
         // deploy it
-        vertx = Vertx.vertx();
+        vertx = Vertx.vertx(); // new VertxOptions().setWorkerPoolSize(1)
         // The number of instances deployed is the number of cores of the machine.
-        int instances = Runtime.getRuntime().availableProcessors();
+        int instances = 1;// Runtime.getRuntime().availableProcessors();
 
         DeploymentOptions opt = new DeploymentOptions().setInstances(instances);
         vertx.deployVerticle(AsyncClientVerticle.class, opt);
@@ -24,8 +29,35 @@ public class AsyncClientVerticle extends AbstractVerticle {
     @Override
     public void start() {
         // find a way to iterate providers, pass to next one by one if first ones failed.
+        this.fetchSequentially();
+//        this.fetchConcurrently();
     }
 
+
+    private void fetchSequentially() {
+        ConcurrentLinkedQueue<DemoDataProvider> demoDataProviderQ = new ConcurrentLinkedQueue<>();
+        IntStream.range(0, 3).forEach(
+            idx -> {
+                DemoDataProvider provider = new DemoDataProvider(idx, vertx);
+                demoDataProviderQ.add(provider);
+            }
+        );
+        Handler<AsyncResult<JsonArray>> finalizeHandler = ar -> {
+            if (ar.succeeded()) {
+                // At least one is succeeded
+                System.out.println(ar.result());
+                // set stored variables, ets..
+            } else {
+                // All failed
+                System.out.println("All failed");
+            }
+        };
+        Future<JsonArray> jsonArrayFuture = Objects.requireNonNull(demoDataProviderQ.poll()).fetchResultFuture(demoDataProviderQ, finalizeHandler);
+    }
+
+    /**
+     * Doesn't work in order
+     */
     private void fetchConcurrently() {
         List<Future> providerFutures = new ArrayList<>();
         IntStream.range(0, 3).forEach(
--- src/main/java/asyncclient/DemoDataProvider.java
+++ src/main/java/asyncclient/DemoDataProvider.java
@@ -1,6 +1,8 @@
 package asyncclient;
 
+import io.vertx.core.AsyncResult;
 import io.vertx.core.Future;
+import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpHeaders;
 import io.vertx.core.json.JsonArray;
@@ -9,7 +11,8 @@ import io.vertx.ext.web.client.WebClient;
 import io.vertx.ext.web.codec.BodyCodec;
 
 import javax.ws.rs.core.MediaType;
-import java.util.concurrent.CompletableFuture;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class DemoDataProvider {
 
@@ -19,6 +22,30 @@ public class DemoDataProvider {
     public DemoDataProvider(int id, Vertx vertx) {
         this.id = id;
         this.client = WebClient.create(vertx);
+    }
+
+    public Future<JsonArray> fetchResultFuture(ConcurrentLinkedQueue<DemoDataProvider> demoDataProviderQ, Handler<AsyncResult<JsonArray>> finalizeHandler) {
+        Future<JsonArray> jsonObjectFuture = Future.future();
+        this.client.get(443, "jsonplaceholder.typicode.com", "/todos")
+            .ssl(true)
+            .putHeader(HttpHeaders.CONTENT_TYPE.toString(), MediaType.APPLICATION_JSON)
+            .as(BodyCodec.jsonArray())
+            .send(ar -> {
+                if (ar.succeeded() && (this.id == 2)) { // only 2 will success
+                    HttpResponse<JsonArray> response = ar.result();
+                    JsonArray jsonArr = response.body();
+                    System.out.println("Got HTTP response body by " + this.id + " with length " + jsonArr.size());
+                    jsonObjectFuture.setHandler(finalizeHandler);
+                    jsonObjectFuture.complete(jsonArr);
+                } else {
+                    String msg = "Failed at " + this.id + ", passing to next";
+                    System.out.println(msg);
+                    jsonObjectFuture.compose(jsonArray -> Objects.requireNonNull(demoDataProviderQ.poll()).fetchResultFuture(demoDataProviderQ, finalizeHandler));
+                    jsonObjectFuture.complete(new JsonArray()); // or no param as null, anyway it'll not be used by composed next future
+                    // no need to set finalize handler if passing to next
+                }
+            });
+        return jsonObjectFuture;
     }
 
     public Future<JsonArray> fetchResultFuture() {
@@ -36,7 +63,7 @@ public class DemoDataProvider {
                 } else {
                     String msg = "Failed at " + this.id;
                     System.out.println(msg);
-                    jsonObjectFuture.fail(new RuntimeException(msg));
+                    jsonObjectFuture.fail(new RuntimeException(msg)); //ar.cause()
                 }
             });
         return jsonObjectFuture;
